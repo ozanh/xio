b5d9834a6273501b77beb15a79664b8a96c4c30b
Ozan Hacıbekiroğlu <ozanhaci@gmail.com>
2023-05-19 22:05:56 +0300

    refactor: minor refactoring - added examples - added README - removed unneccessary exported error - misc. minor changes

---
+++ README.md
@@ -0,0 +1,17 @@
+# xio
+
+[![Go Reference](https://pkg.go.dev/badge/github.com/ozanh/xio.svg)](https://pkg.go.dev/github.com/ozanh/xio)
+[![Go](https://github.com/ozanh/xio/actions/workflows/go.yml/badge.svg)](https://github.com/ozanh/xio/actions/workflows/go.yml)
+
+`xio` is an extended io library for my various needs.
+As of now, It provides `BufPipe`, which is a buffered pipe implementation, and helper `Storage` implementations.
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request if you would like to contribute code.
+
+## License
+
+xio is licensed under the [MIT License](https://github.com/ozanh/xio/blob/main/LICENSE).
+
+Feel free to reach out if you have any questions or feedback!
---
+++ example_test.go
@@ -0,0 +1,127 @@
+package xio_test
+
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/ozanh/xio"
+)
+
+func ExampleBufPipe_example1() {
+	const blockSize = 512
+	const storageSize = 1024 * 1024
+
+	storage := xio.NewBlockStorageBuffer(blockSize, storageSize)
+	pr, pw := xio.BufPipe(blockSize, storageSize, storage)
+
+	go func() {
+		pw.Write([]byte("hello"))
+		pw.Close()
+	}()
+
+	buf := make([]byte, 1024)
+	n, err := pr.Read(buf)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", buf[:n])
+
+	// Output: hello
+}
+
+func ExampleBufPipe_example2() {
+	const blockSize = 1024 * 1024
+	const storageSize = 10 * 1024 * 1024
+
+	storage := xio.NewBlockStorageBuffer(blockSize, storageSize)
+	pr, pw := xio.BufPipe(blockSize, storageSize, storage)
+
+	go func() {
+		src := io.LimitReader(rand.Reader, storageSize)
+		_, err := io.Copy(pw, src)
+		pw.CloseWithError(err)
+	}()
+
+	n, err := io.Copy(io.Discard, pr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%d", n)
+
+	// Output: 10485760
+}
+
+func ExampleBufPipe_example3() {
+	const blockSize = 1024 * 1024
+	const storageSize = 10 * 1024 * 1024
+
+	storage := xio.NewBlockStorageBuffer(blockSize, storageSize)
+	pr, pw := xio.BufPipe(blockSize, storageSize, storage)
+
+	go func() {
+		src := io.LimitReader(rand.Reader, storageSize)
+		_, err := io.Copy(pw, src)
+		pw.CloseWithError(err)
+	}()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	done := make(chan struct{})
+
+	go func() {
+		select {
+		case <-ctx.Done():
+			pw.CloseWithError(ctx.Err())
+		case <-done:
+		}
+	}()
+
+	n, err := io.Copy(io.Discard, pr)
+
+	close(done)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%d", n)
+
+	// Output: 10485760
+}
+
+func ExampleBufPipe_example4() {
+	const blockSize = 1024 * 1024
+	const storageSize = 10 * 1024 * 1024
+
+	storage, err := os.CreateTemp("", "storage")
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(storage.Name())
+	defer storage.Close()
+
+	err = storage.Truncate(storageSize)
+	if err != nil {
+		panic(err)
+	}
+
+	pr, pw := xio.BufPipe(blockSize, storageSize, storage)
+
+	go func() {
+		src := io.LimitReader(rand.Reader, 40_000_000)
+		_, err := io.Copy(pw, src)
+		pw.CloseWithError(err)
+	}()
+
+	n, err := io.Copy(io.Discard, pr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%d", n)
+
+	// Output: 40000000
+}
